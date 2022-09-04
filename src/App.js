7fbab153e2d3f0484bed31685476b02adac2ff75
elantsev1 <elantsev.pa@gmail.com>
2022-09-04 14:25:33 +0300

    2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <b>МОЙ ПРОЕКТ</b>
+        <b>МОЙ ПРОЕКТ 2</b>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
